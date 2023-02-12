447e9c566e399d8dd78390938a21760a47d3b651
naz <safinaz.bilgic@gmail.com>
2023-02-12 00:39:34 +0000

    proje is ready

---
+++ Project-101/First_Part/script.sh
@@ -0,0 +1,4 @@
+cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-[a-zA-Z0-9]{17}" > /tmp/result.txt
+
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminate" | grep -Eo "i-.{17}" | sort | uniq # I did this
